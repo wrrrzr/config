b8ef4aa935f07b446989a359791ad67f1af8c3bf
wrrrzr <wozrer@proton.me>
2024-09-29 12:00:25 +0300

    Add spoofdpi

--- install.sh
+++ install.sh
@@ -1,8 +1,20 @@
 #!/bin/env sh
 
+set -e
+
+which wget systemctl
+
 rm -rf ~/.config/sway ~/.config/foot ~/.config/nvim
 
 cp -r sway ~/.config/sway
 cp -r nvim ~/.config/nvim
 cp -r foot ~/.config/foot
 cp .bashrc ~/.bashrc
+
+cp spoofdpi.service ~/.config/systemd/user/spoofdpi.service
+wget https://github.com/xvzc/SpoofDPI/releases/download/v0.12.0/spoofdpi-linux-amd64.tar.gz
+tar xf spoofdpi-linux-amd64.tar.gz
+rm spoofdpi-linux-amd64.tar.gz
+mv spoofdpi ~
+systemctl --user daemon-reload
+systemctl --user enable --now spoofdpi.service
