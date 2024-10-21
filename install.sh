51ae55268e0e2fe17b5e93bbcc0e49a2fa3292d2
wrrrzr <wozrer@proton.me>
2024-10-21 15:31:14 +0300

    Delete unused

--- install.sh
+++ install.sh
@@ -2,19 +2,9 @@
 
 set -e
 
-which wget systemctl
-
 rm -rf ~/.config/sway ~/.config/kitty ~/.config/nvim
 
 cp -r sway ~/.config/sway
 cp -r nvim ~/.config/nvim
 cp -r kitty ~/.config/kitty
 cp .bashrc ~/.bashrc
-
-cp spoofdpi.service ~/.config/systemd/user/spoofdpi.service
-wget https://github.com/xvzc/SpoofDPI/releases/download/v0.12.0/spoofdpi-linux-amd64.tar.gz
-tar xf spoofdpi-linux-amd64.tar.gz
-rm spoofdpi-linux-amd64.tar.gz
-mv spoofdpi ~
-systemctl --user daemon-reload
-systemctl --user enable --now spoofdpi.service
