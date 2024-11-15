712d11210d2da7adf6eb9a7cbe41bb556c6fd55f
wozrer <wozrer@proton.me>
2024-11-15 21:40:45 +0300

    Update

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,9 +1,10 @@
 require("conform").setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-    python = { "black" },
-  },
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_fallback = true}
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "autoflake", "black", "isort" },
+	},
+	format_on_save = {
+		timeout_ms = 1000,
+		lsp_fallback = true,
+	},
 })
