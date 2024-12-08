67bf3d28050eb4685ae11847845620edfa61cb1a
wrrrzr <wozrer@proton.me>
2024-12-09 00:00:52 +0300

    Update

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-rm -rf ~/.config/sway ~/.config/kitty ~/.config/nvim
+rm -rf ~/.config/sway ~/.config/kitty ~/.config/nvim ~/.config/swaylock
 
 cp -r sway ~/.config/sway
+cp -r swaylock ~/.config/swaylock
 cp -r nvim ~/.config/nvim
 cp -r kitty ~/.config/kitty
 cp .bashrc ~/.bashrc
