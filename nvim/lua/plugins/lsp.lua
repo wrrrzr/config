f388befea221ec199d7d9dbeff6c495ec0dfbd13
wrrrzr <wozrer@proton.me>
2025-01-12 00:28:16 +0300

    Update

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -5,3 +5,28 @@ lspconfig.pylsp.setup({
 lspconfig.clangd.setup({
 	filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
 })
+lspconfig.lua_ls.setup({
+	on_init = function(client)
+		if client.workspace_folders then
+			local path = client.workspace_folders[1].name
+			if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+				return
+			end
+		end
+
+		client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+			runtime = {
+				version = "LuaJIT",
+			},
+			workspace = {
+				checkThirdParty = false,
+				library = {
+					vim.env.VIMRUNTIME,
+				},
+			},
+		})
+	end,
+	settings = {
+		Lua = {},
+	},
+})
