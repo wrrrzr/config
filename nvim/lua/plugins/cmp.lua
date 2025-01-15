3f234fa355085138ca89971891c07dcf15ca14f9
wrrrzr <wozrer@proton.me>
2025-01-15 16:06:38 +0300

    Fix

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -2,11 +2,12 @@ local cmp = require("cmp")
 
 cmp.setup({
 	snippet = {
+		-- REQUIRED - you must specify a snippet engine
 		expand = function(args)
-			local func = vim.fn["vsnip#anonymous"]
-			if func ~= nil then
-				func(args.body)
-			end
+			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+			-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
 		end,
 	},
 	window = {
