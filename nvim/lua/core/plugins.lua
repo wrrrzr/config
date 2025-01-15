367342c0b627f363cc5198422702f78c731bc2e9
wozrer <wozrer@proton.me>
2025-01-15 16:06:38 +0300

    Fix

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -17,11 +17,15 @@ require("lazy").setup({
 	{ "stevearc/oil.nvim", dependencies = { "nvim-tree/nvim-web-devicons" }, lazy = false },
 	{ "akinsho/bufferline.nvim", dependencies = "nvim-tree/nvim-web-devicons" },
 	{ "neovim/nvim-lspconfig" },
+
 	{ "hrsh7th/cmp-nvim-lsp" },
 	{ "hrsh7th/cmp-buffer" },
 	{ "hrsh7th/cmp-path" },
 	{ "hrsh7th/cmp-cmdline" },
 	{ "hrsh7th/nvim-cmp" },
+	{ "hrsh7th/cmp-vsnip" },
+	{ "hrsh7th/vim-vsnip" },
+
 	{ "stevearc/conform.nvim" },
 	{ "Mofiqul/vscode.nvim" },
 	{
