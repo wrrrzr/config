54009fb3dfadf3e070108b14c45d1206b387b3e8
wrrrzr <wozrer@proton.me>
2025-02-24 20:22:30 +0300

    Update

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -23,3 +23,8 @@ map({ "n", "i", "v" }, "<Up>", "<nop>")
 map({ "n", "i", "v" }, "<Down>", "<nop>")
 map({ "n", "i", "v" }, "<Left>", "<nop>")
 map({ "n", "i", "v" }, "<Right>", "<nop>")
+
+map("v", "<C-]>", ":Gen Complete<CR>")
+map("n", "[", function()
+	require("gen").select_model()
+end)
