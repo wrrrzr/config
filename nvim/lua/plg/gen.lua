39e8fe0e42a8ebfc902f5f841914a2b7b43bc596
wrrrzr <wozrer@proton.me>
2025-03-07 19:19:02 +0300

    Fix

--- nvim/lua/plg/gen.lua
+++
@@ -1,6 +0,0 @@
-require('gen').prompts['Complete'] = {
-    prompt =
-    "Regarding the following code, Complete this code, only output the result in format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
-    replace = true,
-    extract = "```$filetype\n(.-)```",
-}
