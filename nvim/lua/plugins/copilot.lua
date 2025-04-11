d4d174d57f2dd3aaf84f82c866dd9f6cc4c14d8a
wrrrzr <wozrer@proton.me>
2025-04-12 00:16:00 +0300

    Add copilot

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,12 @@
+return {}
+
+--[[return {
+	{
+		"zbirenbaum/copilot-cmp",
+		config = function()
+			require("copilot").setup()
+			require("copilot_cmp").setup()
+		end,
+		dependencies = { "zbirenbaum/copilot.lua" },
+	},
+}]]
