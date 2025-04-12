f41d4ca70494eafb9c3114fe66fb56e69204cc9f
wrrrzr <wozrer@proton.me>
2025-04-12 19:34:10 +0300

    Fix

--- nix/home.nix
+++ nix/home.nix
@@ -15,6 +15,23 @@
     pkgs.kitty
     pkgs.luanti
     pkgs.code-cursor
+    pkgs.neovim
+
+    pkgs.lua-language-server
+    pkgs.python313Packages.python-lsp-server
+    pkgs.clang-tools
+    pkgs.rust-analyzer
+    pkgs.cmake-language-server
+    pkgs.gopls
+
+    pkgs.stylua
+    pkgs.black
+    pkgs.python313Packages.autoflake
+    pkgs.isort
+
+    pkgs.ripgrep
+    pkgs.wl-clipboard
+    pkgs.xclip
   ];
 
   home.sessionVariables = {
@@ -49,27 +66,6 @@
     enable = true;
     userEmail = "wozrer@proton.me";
     userName = "wrrrzr";
-  };
-
-  programs.neovim = {
-    enable = true;
-    extraPackages = with pkgs; [
-      lua-language-server
-      python313Packages.python-lsp-server
-      clang-tools
-      rust-analyzer
-      cmake-language-server
-      gopls
-
-      stylua
-      black
-      python313Packages.autoflake
-      isort
-
-      ripgrep
-      wl-clipboard
-      xclip
-    ];
   };
 
   programs.home-manager.enable = true;
