6404bb851188f1f50f4103c326f82d026aa33b48
wrrrzr <wozrer@proton.me>
2025-04-14 16:26:12 +0300

    Update sound nixos

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -55,8 +55,11 @@
 
   time.timeZone = "Europe/Moscow";
 
+  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
   };
  
