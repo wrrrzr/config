b783078e3cc3ce919f70314059bf7119a8416b9b
wrrrzr <wozrer@proton.me>
2025-04-16 13:48:06 +0300

    Delete unsued mappings

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -15,8 +15,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		bufmap("n", "gd", vim.lsp.buf.definition)
 		bufmap("n", "gD", vim.lsp.buf.declaration)
 		bufmap("n", "gi", vim.lsp.buf.implementation)
-		bufmap("n", "gr", vim.lsp.buf.rename)
-		bufmap({ "n", "v" }, "<C-a>", vim.lsp.buf.hover)
 	end,
 })
 
