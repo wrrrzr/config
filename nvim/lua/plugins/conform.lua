f0623662d1936e4326cf540f1fe6332c75a73521
wrrrzr <wozrer@proton.me>
2025-04-18 21:09:12 +0300

    Add auto nixfmt

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -6,6 +6,7 @@ return {
 				formatters_by_ft = {
 					lua = { "stylua" },
 					python = { "autoflake", "black", "isort" },
+					nix = { "nixfmt" },
 				},
 				format_on_save = {
 					timeout_ms = 5000,
