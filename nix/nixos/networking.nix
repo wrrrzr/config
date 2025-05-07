cc7163ed3fef0f29b18bf75aef5a85a932c18623
wrrrzr <wozrer@proton.me>
2025-05-07 13:18:34 +0300

    Add kdeconnect

--- nix/nixos/networking.nix
+++ nix/nixos/networking.nix
@@ -4,6 +4,8 @@
   networking.hostName = "mypc";
   networking.networkmanager.enable = true;
 
+  networking.firewall.enable = false;
+
   networking.nameservers =
     [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
 
