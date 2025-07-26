c849e514db427a203bd99c8f2bc1be59a4aa5396
wrrrzr <wozrer@proton.me>
2025-07-26 19:06:47 +0300

    Change telegram to dino

--- nix/home-manager/gnome.nix
+++ nix/home-manager/gnome.nix
@@ -5,7 +5,7 @@
   dconf.settings = {
     "org/gnome/shell" = {
       favorite-apps =
-        [ "firefox.desktop" "kitty.desktop" "org.telegram.desktop.desktop" ];
+        [ "firefox.desktop" "kitty.desktop" "im.dino.Dino.desktop" ];
     };
     "org/gnome/desktop/wm/keybindings" = {
       switch-to-workspace-1 = [ "<Alt>1" ];
