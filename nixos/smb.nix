9c7b92d0049083c12185fd0fa7d39b9cc94a8883
wrrrzr <wozrer@proton.me>
2025-08-08 12:35:10 +0300

    Update smbmount

--- nixos/smb.nix
+++ nixos/smb.nix
@@ -8,11 +8,15 @@ in
     enable = lib.mkEnableOption "Mount smb mount in me dir";
   };
   config = lib.mkIf cfg.enable {
-    fileSystems."/home/me/notes" = {
+    fileSystems."/home/me/mm" = {
       device = "//10.0.0.5/notes";
       fsType = "cifs";
       depends = [ "wg-quick@wg0.service" ];
-      options = [ "credentials=/etc/nixos/smb-secrets,uid=1000,gid=1000" ];
+      options =
+        let
+          automount = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+        in
+        [ "${automount},credentials=/etc/nixos/smb-secrets,uid=1000,gid=1000" ];
     };
   };
 }
