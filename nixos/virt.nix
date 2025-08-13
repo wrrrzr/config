5977f28c0307e5b7b8b402e454599a9855a3cbee
wrrrzr <wozrer@proton.me>
2025-08-13 15:39:16 +0300

    Add users opt to virt

--- nixos/virt.nix
+++ nixos/virt.nix
@@ -11,11 +11,18 @@ in
 {
   options.module.virt = {
     enable = lib.mkEnableOption "Virt manager";
+    users = lib.mkOption {
+      type = with lib.types; listOf (passwdEntry str);
+      default = [ ];
+      description = "Users that can use virt-manager";
+    };
   };
   config = lib.mkIf cfg.enable {
     programs.virt-manager.enable = true;
 
     virtualisation.libvirtd.enable = true;
+
+    users.groups.libvirtd.members = cfg.users;
 
     virtualisation.spiceUSBRedirection.enable = true;
   };
