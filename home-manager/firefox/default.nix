247c61403bd7e85bc880d0534185ac908d8fef09
wrrrzr <wozrer@proton.me>
2025-08-14 22:28:29 +0300

    Add mwmbl

--- home-manager/firefox/default.nix
+++ home-manager/firefox/default.nix
@@ -1,33 +1,15 @@
 {
-  pkgs,
   firefox-addons,
   system,
   ...
 }:
 
 {
+  imports = [ ./search.nix ];
+
   programs.firefox = {
     enable = true;
     profiles.me = {
-      search.engines."Nix Packages" = {
-        urls = [
-          {
-            template = "https://search.nixos.org/packages";
-            params = [
-              {
-                name = "type";
-                value = "packages";
-              }
-              {
-                name = "query";
-                value = "{searchTerms}";
-              }
-            ];
-          }
-        ];
-        icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-        definedAliases = [ "@nixpkgs" ];
-      };
       extensions.packages = with firefox-addons.packages.${system}; [
         multi-account-containers
         darkreader
---
+++ home-manager/firefox/search.nix
@@ -0,0 +1,50 @@
+{
+  pkgs,
+  ...
+}:
+
+{
+  programs.firefox = {
+    profiles.me = {
+      search.engines = {
+        "Nix Packages" = {
+          urls = [
+            {
+              template = "https://search.nixos.org/packages";
+              params = [
+                {
+                  name = "type";
+                  value = "packages";
+                }
+                {
+                  name = "query";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+          definedAliases = [ "@nixpkgs" ];
+        };
+        "Mwmbl" = {
+          urls = [
+            {
+              template = "https://mwmbl.org/";
+              params = [
+                {
+                  name = "q";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          icon = pkgs.fetchurl {
+            url = "https://mwmbl.org/static/images/logo.svg";
+            sha256 = "YqhPROWFxaZs+YXmXsUaEt8/fzx+VUJLb4tRc9qik0s=";
+          };
+          definedAliases = [ "@mwmbl" ];
+        };
+      };
+    };
+  };
+}
