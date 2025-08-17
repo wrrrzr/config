c91547e655580a26344a757d2fff59e720df4264
wrrrzr <wozrer@proton.me>
2025-08-17 18:29:26 +0300

    Add nixos installer

---
+++ installer/default.nix
@@ -0,0 +1,41 @@
+{
+  pkgs,
+  modulesPath,
+  ...
+}:
+
+{
+  imports = [
+    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+  ];
+
+  environment.systemPackages = with pkgs; [
+    git
+  ];
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  users.users.installer = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIxVzlnNIpKHwyy6Yw5lctgo0JplO0AXtuiDYVzy5A0s"
+    ];
+  };
+
+  environment.enableAllTerminfo = true;
+
+  security.sudo.wheelNeedsPassword = false;
+
+  networking.firewall.enable = false;
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+    };
+  };
+}
