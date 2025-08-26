3f80b6a9bf90f5d48b26831ab27cddac9157e326
wrrrzr <wozrer@proton.me>
2025-08-26 22:53:12 +0300

    Change grub to systemd-boot

--- machines/mypc/boot.nix
+++ machines/mypc/boot.nix
@@ -3,10 +3,7 @@
 }:
 
 {
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.efiSupport = true;
-  boot.loader.efi.efiSysMountPoint = "/boot";
+  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.tmp.useTmpfs = true;
 }
