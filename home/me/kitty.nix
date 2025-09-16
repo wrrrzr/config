78f00495080e1b43c256ec95303faf86edb6f27a
wrrrzr <wozrer@proton.me>
2025-09-16 22:34:55 +0300

    Add kitty integrations

--- home/me/kitty.nix
+++ home/me/kitty.nix
@@ -6,6 +6,8 @@
     settings = {
       confirm_os_window_close = -1;
     };
+    shellIntegration.enableBashIntegration = true;
+    enableGitIntegration = true;
     font = {
       package = pkgs.nerd-fonts.jetbrains-mono;
       name = "JetBrainsMonoNL NF";
