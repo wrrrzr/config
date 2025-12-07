ba5f2320020c4b1674a0710aa867f85c498bf505
wrrrzr <wozrer@proton.me>
2025-12-07 20:45:47 +0300

    Add iwgtk bind

--- home/me/sway.nix
+++ home/me/sway.nix
@@ -129,6 +129,7 @@ in
         "Print" = "exec ${pkgs.grim}/bin/grim - | ${pkgs.wl-clipboard}/bin/wl-copy --type image/png";
         "${mod}+Shift+p" = "exec ${pkgs.pavucontrol}/bin/pavucontrol";
         "${mod}+Shift+b" = "exec ${pkgs.blueman}/bin/blueman-manager";
+        "${mod}+Shift+i" = "exec ${pkgs.iwgtk}/bin/iwgtk";
       }
       // workspace-binds;
       modes = {
