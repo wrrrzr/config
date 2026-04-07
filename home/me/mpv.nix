1af3a2456be65b37bad19fdd66d9d833d278fdd2
wrrrzr <wozrer@proton.me>
2026-04-07 03:03:16 +0300

    Update mpv config

--- home/me/mpv.nix
+++ home/me/mpv.nix
@@ -1,10 +1,18 @@
 { ... }:
 
+let
+  mpv = {
+    no = "no";
+    yes = "yes";
+  };
+in
 {
   programs.mpv = {
     enable = true;
     config = {
-      no-audio-display = "";
+      audio-display = mpv.no;
+      deband = mpv.yes;
+      save-position-on-quit = mpv.yes;
     };
   };
 }
